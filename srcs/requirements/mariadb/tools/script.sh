918ad3a151055acf86555e84943e7917f30a22e3
yahamdan <yassinehamdani08@gmail.com>
2024-06-04 14:40:12 +0100

    inception

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+
 service mariadb start 
-sleep 2
+
+sleep 1
+
 mysql -e "CREATE DATABASE IF NOT EXISTS $db_name ;"
 
-mysql -e "CREATE USER IF NOT EXISTS '$db_user'@'locahost' IDENTIFIED BY '$db_pwd';"
+mysql -e "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd';"
 
-mysql -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'localhost' IDENTIFIED BY '$db_pwd';"
+mysql -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' IDENTIFIED BY '$db_pwd';"
 
 mysql -e "FLUSH PRIVILEGES;"
 
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-sleep 8
-cd /var/www/html
+sleep 3
 
 wp core download --allow-root
 
-# mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 wp config create --dbname=$db_name --dbuser=$db_user --dbpass=$db_pwd --dbhost=$db_host --allow-root
-wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+
+wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --allow-root
+
 wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
 
-
-# uses the sed command to modify the www.conf file in the /etc/php/7.4/fpm/pool.d directory. The s/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g command substitutes the value 9000 for /run/php/php7.4-fpm.sock throughout the file. This changes the socket that PHP-FPM listens on from a Unix domain socket to a TCP port.
-sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
-
-# creates the /run/php directory, which is used by PHP-FPM to store Unix domain sockets.
 mkdir /run/php
 
-php-fpm7.4 -F
+/usr/sbin/php-fpm7.4 -F
